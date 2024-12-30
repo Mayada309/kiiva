3a0d721369acfa478dc6913b5b23e2ba716fd73a
Mayada <mayadaelsayed383@gmail.com>
2024-12-30 17:45:11 +0200

    create new types

--- app/types.ts
+++ app/types.ts
@@ -1,18 +1,40 @@
 import { StaticImageData } from 'next/image';
 
-export type Product = {
+type Option = {
+  id: number;
+  value: string;
+};
+
+type Sku = {
+  id: number;
+  product_id: number;
+  code: string;
+  quantity: number;
+  is_in_stock: boolean;
+  price: number;
+  compare_price: number;
+  images: StaticImageData[];
+  options: Option[];
+};
+
+export type PublicProduct = {
   id: number;
   name: string;
-  price: number;
   description: string;
-  imageUrl: StaticImageData;
+  short_description: string;
+  is_active: boolean;
+  is_featured: boolean;
+  is_new: boolean;
+  price: number;
+  compare_price: number;
+  images: StaticImageData[];
+  skus: Sku[];
   category: string;
   sizeOptions: string[];
   color: string;
   stock: number;
   amount: number;
   selectedSize: string;
-  featured: boolean;
 };
 
 export type NavLink = {
@@ -25,4 +47,43 @@ export type City = {
   id: number;
   name: string;
   shippingFee: number;
-}
+};
+
+type ParentCategory = {
+  id: number;
+  name: string;
+  slug: string;
+};
+type SubCategory = {
+  id: number;
+  name: string;
+  parent_id: number;
+  is_parent: false;
+};
+export type Category = {
+  id: number;
+  name: string;
+  slug: string;
+  is_parent: boolean;
+  children_count: number;
+  products_count: number;
+  parent?: ParentCategory;
+  children?: SubCategory[];
+};
+
+type MetaLink = {
+  url: string | null;
+  label: string;
+  active: boolean;
+};
+
+export type PaginationData = {
+  current_page: number;
+  from: number;
+  last_page: number;
+  links: MetaLink[];
+  path: string;
+  per_page: number;
+  to: number;
+  total: number;
+};
