3a4c1f1bebaf1b8d7c7b6d309f124885f1500b6a
Mayada <mayadaelsayed383@gmail.com>
2024-12-30 17:43:32 +0200

    handle no products case

--- components/products/ProductsList.tsx
+++ components/products/ProductsList.tsx
@@ -2,13 +2,15 @@ import { Product } from '@/app/types';
 import ProductCard from './ProductCard';
 import Container from '../global/Container';
 
-function ProductsList({ products }: { products: Product[] }) {
+function ProductsList({ products }: { products?: Product[] }) {
   return (
     <Container className='flex items-center justify-center my-8'>
       <ul className='grid grid-cols-1 items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
-        {products.map((product) => {
-          return <ProductCard key={product.id} product={product} />;
-        })}
+        {products
+          ? products.map((product) => {
+              return <ProductCard key={product.id} product={product} />;
+            })
+          : 'no products found'}
       </ul>
     </Container>
   );
